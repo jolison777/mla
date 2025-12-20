723d1d46af7312b1c254cfd4c8556caa91c10447
jolison777 <jolisonraj7@gmail.com>
2025-12-20 15:11:15 +0530

    Holaaa

--- src/mla/example.java
+++ src/mla/example.java
@@ -4,7 +4,7 @@ public class example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hollahjvjvhaaa");
+System.out.print("Hoghchfhjfjfyllaa");
 	}
 
 }
